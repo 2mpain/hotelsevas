efafd97c165693ab63ab7dbdcba15dde99ab4ef6
2mpain <slavianin898@gmail.com>
2023-12-21 17:40:10 +0300

    feat: light theme text colors

--- src/components/ContactUs/contactus.tsx
+++ src/components/ContactUs/contactus.tsx
@@ -4,7 +4,7 @@ import { Button } from "../readyToUse/button";
 
 const ContactUs: React.FC = () => {
   return (
-    <div className="bg-transparent text-white flex flex-col justify-center py-10 w-screen min-h-screen">
+    <div className="dark:bg-transparent light:text-black dark:text-white flex flex-col justify-center py-10 w-screen min-h-screen">
       <Header title="Свяжитесь с нами" id="contactus" />
       <div className=" flex flex-col justify-center items-center">
         <form className="flex flex-col gap-4 mt-16 px-10 lg:mt-20 min-w-full lg:min-w-[500px]">
@@ -15,7 +15,7 @@ const ContactUs: React.FC = () => {
             maxLength={128}
             type="text"
             placeholder="Ваше имя"
-            className="bg-gray-50 text-gray-900 outline-none border-2 border-white rounded-3xl px-8 py-2"
+            className="bg-gray-300 text-gray-900 outline-none border-2 border-white rounded-3xl px-8 py-2"
           />
           <input
             id="email"
@@ -24,7 +24,7 @@ const ContactUs: React.FC = () => {
             required
             maxLength={128}
             placeholder="Ваша почта"
-            className="bg-gray-50 text-gray-900 outline-none border-2 border-white rounded-3xl px-8 py-2"
+            className="bg-gray-300 text-gray-900 outline-none border-2 border-white rounded-3xl px-8 py-2"
           />
           <textarea
             id="message"
@@ -32,7 +32,7 @@ const ContactUs: React.FC = () => {
             required
             maxLength={12334234}
             placeholder="Чем можем Вам помочь?"
-            className="bg-gray-50 text-gray-900 outline-none border-2 border-white rounded-3xl px-8 py-6 min-h-[16em]"
+            className="bg-gray-300 text-gray-900 outline-none border-2 border-white rounded-3xl px-8 py-6 min-h-[16em]"
           />
           <div className="text-center mt-10">
             <Button
