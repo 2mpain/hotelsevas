61a2984d2b64e003e6d6975dc33a63e2a16f7569
2mpain <slavianin898@gmail.com>
2023-12-21 17:33:22 +0300

    feat: system theme remove

--- src/components/readyToUse/drawer.tsx
+++ src/components/readyToUse/drawer.tsx
@@ -1,31 +1,26 @@
-import * as React from 'react';
-import Box from '@mui/joy/Box';
-import Drawer from '@mui/joy/Drawer';
-import { Button } from "@/components/readyToUse/button"
-import List from '@mui/joy/List';
-import Divider from '@mui/joy/Divider';
-import ListItem from '@mui/joy/ListItem';
-import ListItemButton from '@mui/joy/ListItemButton';
-import { Menu } from 'lucide-react';
+import * as React from "react";
+import Box from "@mui/joy/Box";
+import Drawer from "@mui/joy/Drawer";
+import { Button } from "@/components/readyToUse/button";
+import List from "@mui/joy/List";
+import Divider from "@mui/joy/Divider";
+import ListItem from "@mui/joy/ListItem";
+import ListItemButton from "@mui/joy/ListItemButton";
+import { Menu } from "lucide-react";
 import "../../index.css";
 import { useTheme } from "@/components/readyToUse/theme-provider";
-import {useNavigate} from 'react-router-dom'
-
 
 export default function DrawerComp() {
-
-  const navigate = useNavigate();
-  
   const [open, setOpen] = React.useState(false);
 
   const { theme } = useTheme();
-  
+
   const toggleDrawer =
     (inOpen: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
       if (
-        event.type === 'keydown' &&
-        ((event as React.KeyboardEvent).key === 'Tab' ||
-          (event as React.KeyboardEvent).key === 'Shift')
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
       ) {
         return;
       }
@@ -33,55 +28,58 @@ export default function DrawerComp() {
       setOpen(inOpen);
     };
 
-
   const drawerItems = [
     {
       id: 1,
-      text: 'Вход',
-      page: 'signIn'
+      text: "Вход",
+      page: "signIn",
     },
     {
       id: 2,
-      text: 'Отзывы клиентов',
-      page: 'feedbacks'
+      text: "Отзывы клиентов",
+      page: "feedbacks",
     },
     {
       id: 3,
-      text: 'Ответы на вопросы',
-      page: 'questions',
+      text: "Ответы на вопросы",
+      page: "questions",
     },
     {
       id: 4,
-      text: 'Как к нам заехать?',
-      page: 'steps'
+      text: "Как к нам заехать?",
+      page: "steps",
     },
     {
       id: 5,
-      text: 'Наши номера',
-      page: 'rooms'
+      text: "Наши номера",
+      page: "rooms",
     },
     {
       id: 6,
-      text: 'Обратная связь',
-      page: 'contactus'
+      text: "Обратная связь",
+      page: "contactus",
     },
-  ]
+  ];
 
   const handleMenuItemClick = (page: string) => {
-    console.log(page)
+    console.log(page);
     setOpen(false);
     const section = document.getElementById(page);
-    if(section){
-      section.scrollIntoView({behavior: "smooth"})
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
     }
-  }
+  };
 
   return (
-    <Box sx={{ }}>
-      <Button size="icon" className='my-2'>
-      <Menu className={`burger ${theme === "dark" ? "dark-burger" : ""}`} color={theme === "dark" ? "black" : "white"} onClick={toggleDrawer(true)} />
+    <Box sx={{}}>
+      <Button size="icon" className="my-2">
+        <Menu
+          className={`burger ${theme === "dark" ? "dark-burger" : ""}`}
+          color={theme === "dark" ? "black" : "white"}
+          onClick={toggleDrawer(true)}
+        />
       </Button>
-      
+
       <Drawer open={open} onClose={toggleDrawer(false)}>
         <Box
           role="presentation"
@@ -90,7 +88,10 @@ export default function DrawerComp() {
         >
           <List>
             {drawerItems.slice(0, 4).map((item) => (
-              <ListItem key={item.id} onClick={() => handleMenuItemClick(item.page)}>
+              <ListItem
+                key={item.id}
+                onClick={() => handleMenuItemClick(item.page)}
+              >
                 <ListItemButton>{item.text}</ListItemButton>
               </ListItem>
             ))}
@@ -98,7 +99,10 @@ export default function DrawerComp() {
           <Divider />
           <List>
             {drawerItems.slice(4).map((item) => (
-              <ListItem key={item.id} onClick={()=> handleMenuItemClick(item.page)}>
+              <ListItem
+                key={item.id}
+                onClick={() => handleMenuItemClick(item.page)}
+              >
                 <ListItemButton>{item.text}</ListItemButton>
               </ListItem>
             ))}
